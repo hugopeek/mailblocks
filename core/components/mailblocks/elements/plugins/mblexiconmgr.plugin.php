0a76c27131fc3a2623f64d20adb388b948c1b869
Hugo Peek <hugo@fractal-farming.com>
2021-08-02 23:00:03 +0800

    Load MailBlocks lexicon in MODX manager

---
+++ core/components/mailblocks/elements/plugins/mblexiconmgr.plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * mbLexiconManager
+ *
+ * Load MailBlocks lexicon in MODX manager.
+ *
+ * @var modX $modx
+ * @package mailblocks
+ */
+
+$modx->controller->addLexiconTopic('mailblocks:default');
---
+++ core/components/mailblocks/lexicon/en/default.inc.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Default English lexicon entries for MailBlocks
+ *
+ * @package mailblocks
+ * @subpackage lexicon
+ */
+
+// General
+//-----------------------------------------------------
+
+$_lang['mailblocks'] = 'MailBlocks';
+
+
+// Manager context
+//-----------------------------------------------------
+
+// Menus
+$_lang['mailblocks.menu.edit_forms_title'] = 'MailBlocks';
+$_lang['mailblocks.menu.edit_forms_description'] = 'Build and edit your own HTML emails';
+
+// Collection views
+$_lang['mailblocks.collections'] = 'Overview of all your MailBlocks emails';
+$_lang['mailblocks.collections.children'] = 'Emails';
+$_lang['mailblocks.collections.children.create'] = 'Create Email';
+$_lang['mailblocks.collections.children.back_to_collection_label'] = 'All Emails';
+
+
+// Web context
+//-----------------------------------------------------
+
